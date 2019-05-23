c033db84029ed6ab7d4fba3fbe7d31853fc9f7d4
Stephen Homick <shomick@datto.com>
2019-05-23 14:35:37 -0400

    push

--- geoLocate.sh
+++ geoLocate.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+### Requires jq, curl and dig to be installed. ###
+#
 function geo(){
     for args in $@ ; do curl -s ipinfo.io/$(dig +short "$args") | jq;done
 }
-geo "$@"
 
+if [ $# -eq 0 ] ; then
+    echo "No arguments supplied. Exiting"
+else
+    geo "$@"
+fi
 
